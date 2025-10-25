be0fb10a27c73161c39b487163309c4091c83bba
황효주 <hjoo830@naver.com>
2025-10-25 22:19:15 +0900

    feat: add JpaMemberRepository

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY: DB가 알아서 생성해주는거
     private Long id; // 고객이 지정한 id 아니고 시스템이 지정하는 id
     private String name;
 
